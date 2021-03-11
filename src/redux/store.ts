1d73625b1c4ce2d85567203e7f7dc9af15781392
Wendy Chen <jaewchen@gmail.com>
2021-03-11 02:04:01 -0600

    Only loading storage in renderer configure store.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
 import { applyMiddleware, compose, createStore } from 'redux';
+import type * as Redux from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 import { rootReducer } from './reducers';
@@ -11,26 +12,30 @@ export const STORE_SCOPES = {
   RENDERER: 'RENDERER',
 };
 
-export const configureStore = (scope = STORE_SCOPES.MAIN) => {
+export const configureStore = (scope = STORE_SCOPES.MAIN): Redux.Store => {
   const middlewares = [];
   const logger = window.cards.logger && window.cards.logger();
   if (logger) {
     middlewares.push(logger);
   }
 
-  const loadedState = window.cards.storage.load(PERSIST_KEY);
   let persistedState = {};
-  try {
-    persistedState = JSON.parse(loadedState);
-  } catch (error) {
-    console.log(`Parsing localstorage key "${PERSIST_KEY}" failed with ${error}.`);
+  if (scope === STORE_SCOPES.RENDERER) {
+    const loadedState = window.cards.storage.load(PERSIST_KEY);
+    try {
+      persistedState = JSON.parse(loadedState);
+    } catch (error) {
+      console.log(`Parsing localstorage key "${PERSIST_KEY}" failed with ${error}.`);
+    }
   }
 
-  const composeEnhancers = (scope === STORE_SCOPES.RENDERER && window.cards.isDevelopment) ?
+  const composeEnhancers = (scope === STORE_SCOPES.RENDERER) ?
     composeWithDevTools({ trace: true }) : compose;
   const store = createStore(rootReducer, persistedState, composeEnhancers(applyMiddleware(...middlewares)));
 
-  store.subscribe(() => window.cards.storage.save(PERSIST_KEY, PERSIST_WHITELIST, store.getState()));
+  if (scope === STORE_SCOPES.RENDERER) {
+    store.subscribe(() => window.cards.storage.save(PERSIST_KEY, PERSIST_WHITELIST, store.getState()));
+  }
 
   return store;
 };
